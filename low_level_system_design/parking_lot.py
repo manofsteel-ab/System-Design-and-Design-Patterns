91bc4ea87fb0037865edf8df24ef8686f320dca1
Abhishek Kumar Yadav <abhiy.yadav7@gmail.com>
2020-02-03 18:19:28 +0530

    plot

--- low_level_system_design/parking_lot.py
+++ low_level_system_design/parking_lot.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from uuid import UUID
 from time import time
+import threading
 
 
 class VehicleType(Enum):
@@ -65,39 +66,6 @@ class Contact:
         self.sequence = sequence
 
 
-class UserDetails:
-    # TO store user personal details
-    def __init__(self, name, addresses=[], contacts=[]):
-        self.name = name
-        self.addresses = addresses
-        self.contacts = contacts
-
-
-class Account:
-    # to store user account related information
-    def __init__(self, username, password, user_details, status):
-        self.username = username
-        self.password = password
-        self.user_info = user_details
-        self.status = status
-
-
-class Admin(Account):
-    def __init__(
-            self, username, password, user_details,
-            status=AccountStatus.ACTIVE
-    ):
-        super().__init__(username, password, user_details, status)
-
-
-class ParkingAttendant(Account):
-    def __init__(
-            self, username, password, user_details,
-            status=AccountStatus.ACTIVE
-    ):
-        super().__init__(username, password, user_details, status)
-
-
 # Parking spot
 
 class ParkingSpot(ABC):
@@ -110,7 +78,6 @@ class ParkingSpot(ABC):
         self.status = status
         self.base_charge = base_charge
         self.special_charge_per_hour = special_charge
-        self.spot_number = spot_number
         self.type = spot_type
         self.allocated_vehicle = None
 
@@ -229,18 +196,134 @@ class ParkingFloor:
     def get_total_spots(self):
         return len(self.spots)
 
-    def get_available_spots_count(self):
+    def get_available_spots_count(self, spot_type=None):
         count = 0
         for spot in self.spots:
+            if spot_type and spot.type != spot_type:
+                continue
             if spot.get_spot_status() == ParkingSpotStatus.AVAILABLE:
                 count = count+1
 
         return count
 
-    def get_unavailable_spots_count(self):
+    def get_unavailable_spots_count(self, spot_type=None):
         count = 0
         for spot in self.spots:
+            if spot_type and spot.type != spot_type:
+                continue
             if spot.get_spot_status() == ParkingSpotStatus.UNAVAILABLE:
                 count = count + 1
         return count
+
+
+class ParkingLot:
+    #  singleton ParkingLot to ensure only one object of ParkingLot in the
+    #  system
+    instance = None
+
+    class __OnlyOne:
+        def __init__(self, name, floor_limits):
+            self.name = name
+            self.addresses = []
+            self.contacts = []
+            self.floors = []
+            self.floor_sequence_mapping = {}
+            self.floor_limits = floor_limits
+            self.entrance_panels = [1, 2, 3] # for example
+            self.exit_panels = [] = [1, 2, 3] # for example
+            self.lock = threading.Lock()
+
+        def add_plot_address(self, address):
+            self.addresses.append(address)
+
+        def add_contacts(self, contact):
+            self.contacts.append(contact)
+
+        def add_floor(self, floor):
+            if floor.number in self.floor_sequence_mapping:
+                raise Exception('This floor is already present')
+            curr_floor_size = len(self.floors)
+            if curr_floor_size == self.floor_limits:
+                raise Exception('Maximum limit reached')
+            self.floors.append(floor)
+            self.floor_sequence_mapping[floor.number] = curr_floor_size
+
+        def remove_floor(self):
+            # same as spot
+            pass
+
+        def get_free_spot(self, spot_type):
+            pass
+
+    def __init__(self, name, floor_limit):
+        if not ParkingLot.instance:
+            ParkingLot.instance = ParkingLot.__OnlyOne(name, floor_limit)
+        else:
+            ParkingLot.instance.name = name
+            ParkingLot.instance.floor_limit = floor_limit
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+
+class UserDetails:
+    # TO store user personal details
+    def __init__(self, name, addresses=[], contacts=[]):
+        self.name = name
+        self.addresses = addresses
+        self.contacts = contacts
+
+
+class Account:
+    # to store user account related information
+    def __init__(self, username, password, user_details, status):
+        self.username = username
+        self.password = password
+        self.user_info = user_details
+        self.status = status
+        self.parking_lot = ParkingLot(name="XXXX", floor_limit=10)
+
+
+class Admin(Account):
+    def __init__(
+            self, username, password, user_details,
+            status=AccountStatus.ACTIVE
+    ):
+        super().__init__(username, password, user_details, status)
+
+    def add_floor(self, floor_number):
+        floor = ParkingFloor(floor_number, 100)
+        self.parking_lot.add_floor(floor)
+
+    def add_spot(
+            self, floor, spot_number, base_charge, special_charge_per_hour,
+            spot_type
+    ):
+        spot = ParkingSpot(
+            spot_number, ParkingSpotStatus.AVAILABLE, base_charge,
+            special_charge_per_hour, spot_type
+        )
+        floor.add_spot(spot)
+
+
+class ParkingAttendant(Account):
+    def __init__(
+            self, username, password, user_details,
+            status=AccountStatus.ACTIVE
+    ):
+        super().__init__(username, password, user_details, status)
+
+    def generate_ticket(self, vehicle_number, vehicle_type, spot_type):
+        ticket = Ticket()
+        if vehicle_type == VehicleType.CAR:
+            vehicle = Car(vehicle_number, ticket)
+        else:
+            raise Exception
+        spot = self.parking_lot.get_free_spot(spot_type)
+        spot.allocate_vehicle(vehicle)
+        return {
+            'ticket': ticket.ticket_number,
+            'spot': spot.spot_number,
+        }
+
 
