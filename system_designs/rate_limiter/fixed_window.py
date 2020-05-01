7e2fa8f0991aac64317272daa2543f07f2177f19
Abhishek Kr Yadav <abhiy.yadav7@gmail.com>
2020-05-02 04:18:59 +0530

    fixed window

--- system_designs/rate_limiter/fixed_window.py
+++ system_designs/rate_limiter/fixed_window.py
@@ -1,30 +1,53 @@
 from abc import ABC, abstractmethod
+import threading
 
-class RateLimiter(ABC):
+import time
+import random
 
-    _max_request_per_second=None
 
-    def __init__(self, max_request_per_second=0):
-        self._max_request_per_second = max_request_per_second
+# Rate limit(per minut) Configuration api level, user level
+class Configuration:
+    def __init__(self):
+        self.total_call = 50
+        self.api = {'api_1': 10, 'api_2': 10}
+        self.user = {'user_1': 10, 'user_2':20}
+        self.user_api = {
+            'user_1_api_1': 5, 'user_2_api_1':10,
+            'user_1_api_2': 5, 'user_2_api_2':10,
+        }
+
+class RateLimiterInterface(ABC):
+
+    def __init__(self):
+        self.api_db = {} # ex: key=api_1_12:00 and value will be freq
+        self.user_db = {} # ex. key=user_1_12:00 and value will be freq
+        self.user_api_db = {} # ex. key=user_1_api_1_12:00 and value will be freq
+        self.total_call_db = {} # ex. key=12:00 and value will be freq
 
     @abstractmethod
     def allow(self):
         pass
 
-class FixedWindowRateLimiter(RateLimiter):
-    def __init__(self, max_request_per_second=0):
-        super().__init__(max_request_per_second)
-        self._storage = {}
-        print(self._max_request_per_second)
+    def get_current_time(self):
+        return int(time.time())
 
-    def put(self):
-        pass
 
-    def get(self):
-        pass
+
+class FixedWindowRateLimiter(RateLimiterInterface):
+
+    def __init__(self):
+        super().__init__()
 
     def allow(self):
         pass
 
+class SendRequest:
 
-FixedWindowRateLimiter()
+    @classmethod
+    def request_task(cls):
+        users = ['user_1', 'user_2']
+        apis = ['api_1', 'api_2']
+
+        while True:
+            user = (random.randint(1, 100))%2
+            api = (random.randint(1, 100))%2
--- system_designs/rate_limiter/interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class RateLimiter(ABC):
-
-    _maxRequestPerSec=None
-
-    @abstractmethod
-    def allow(self):
-        pass
